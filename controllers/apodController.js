9166cdee65ca984e67f9dcd9d304738b75a0aef3
Jonas Molsted <jonasmolsted@gmail.com>
2025-11-04 13:23:12 +0100

    Auto stash before checking out "HEAD"

--- controllers/apodController.js
+++ controllers/apodController.js
@@ -1,42 +1,43 @@
 const connectDB = require('../config/dbConnection');
 
 async function fetchAndStoreApod() {
-    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=NlRFb0W6YdCdCnPnc1Ewjzh47OXHUaR8dkL4wNdb');
-    const data = await response.json();
-    const db = await connectDB();
+  const response = await fetch('http://api.open-notify.org/iss-now.json');
+  
+  const data = await response.json();
+  const db = await connectDB();
 
-    try {
-        if( await db.collection('apod').findOne({ 'data.date': data.date })) {
-            console.info('Skipped adding APOD - already exists');
-            return;
-        }
+  try {
+      if( await db.collection('apod').findOne({ 'data.timestamp': data.timestamp })) {
+          console.info('Skipped adding APOD - already exists');
+          return;
+      }
 
-        if(!response.ok || data.error) {
-            throw new Error(data.error?.message || 'Fetching APOD failed');
-        }
+      if(!response.ok || data.error) {
+          throw new Error(data.error?.message || 'Fetching APOD failed');
+      }
 
-        await db.collection('apod').insertOne({ data });
-    } catch (error) {
-        console.error(error);
-    } finally {
-        setTimeout(fetchAndStoreApod, 3600000);
-    }
+      await db.collection('spaceStation').insertOne({ data: data.iss_position.latitude });
+  } catch (error) {
+      console.error(error);
+  } finally {
+      setTimeout(fetchAndStoreApod, 10);
+  }
 }
 
 async function getApod(req, res) {
   try {
     const db = await connectDB();
-    const dateParam = req.query.date;
+    const dateParam = req.query.iss_position.latitude;
 
     let query = {};
-    let sort = { 'data.date': -1 };
+    let sort = { 'data.timestamp': -1 };
 
     if (dateParam) {
-      query = { 'data.date': dateParam };
+      query = { 'data.timestamp': dateParam };
       sort = {}; // no sort needed for exact match
     }
 
-    const result = await db.collection('apod')
+    const result = await db.collection('spaceStation')
       .find(query)
       .sort(sort)
       .limit(1)
@@ -46,15 +47,13 @@ async function getApod(req, res) {
       return res.status(404).json({ error: 'No APOD found' });
     }
 
-    const apodData = result[0].data;
-    const url = apodData.url;
-    const date = apodData.date;
-    const explanation = apodData.explanation;
-
+    const spaceStationData = result[0].data;
+    const timestamp = spaceStationData.timestamp;
+    const latitude = spaceStationData.iss_position.latitude;
+    
     return res.json({
-      url: url,
-      date: date,
-      explanation: explanation
+      timestamp: spaceStationData.timeStamp,
+      latitude: spaceStationData.iss_position.latitude
     });
     
   } catch (error) {
@@ -66,4 +65,4 @@ async function getApod(req, res) {
 module.exports = {
     fetchAndStoreApod,
     getApod
-};
+}; 
