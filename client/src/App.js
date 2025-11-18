9370cc8123e5897b6641a61fd5edc8e7144a4b70
Jonas Molsted <jonasmolsted@gmail.com>
2025-11-18 10:51:34 +0100

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import SpaceStation from './components/SpaceStation';
 
-//const ws = new WebSocket('ws://localhost:3500');
-//ws={ws
-
 function App() {
   return (
     <div className="App">
