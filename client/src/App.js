9ca6f5164cf8f77ba537526547fe75eff933a73b
Jonas Molsted <jonasmolsted@gmail.com>
2025-11-18 15:21:46 +0100

    shared ws connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
+import { useState, useEffect } from "react"
 import './App.css';
 import SpaceStation from './components/SpaceStation';
 
 function App() {
+  const [ws, setWs] = useState(null);
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:3500");
+
+    socket.onopen = () => {
+      console.log("Connected to WebSocket server");
+      socket.send(JSON.stringify({ type: "hello", payload: "Frontend connected!" }));
+    };
+
+    socket.onclose = () => {
+      console.log("Disconnected from WebSocket server");
+    };
+
+    socket.onerror = (err) => {
+      console.error("WebSocket error:", err);
+    };
+
+    setWs(socket);
+
+    return () => {
+      socket.close(); // cleanup on unmount (on refresh for example)
+    };
+  }, []);
+
   return (
     <div className="App">
       <h1>Space Station Tracker</h1>
-      <SpaceStation/>
+      { ws ? <SpaceStation ws={ ws }/> : null }
     </div>
   );
 }
