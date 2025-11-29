a578af03bf88f67b52859d9f3479a2ab246cf086
Jonas Molsted <jonasmolsted@gmail.com>
2025-11-29 12:37:14 +0100

    cleanup server script

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
-const corsOptions = require('./config/corsOptions');
 const { startSpaceFetchInterval } = require('./controllers/spaceStationController');
 const connectDB = require('./config/dbConnection');
 const { initWebSocket } = require('./config/webSocket');
@@ -19,14 +17,10 @@ async function startServer() {
     });
 
     // Middleware
-    app.use(cors(corsOptions)); // cross origin resource sharing. Helps during development, because frontend and backend might run on different localhost, if frameworks like react or angular are used
-    app.use(express.urlencoded({ extended: false })); // enables express to parse URL-encoded form data
     app.use(express.json()); // enables express to parse incoming JSON data
-    //app.use(express.static(path.join(__dirname, '/public'))); // serves static and public accessible files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));// serves as static and public accessible files
 
     // Routes
-    //app.use('/', require('./routes/index'));
     app.use('/employees', require('./routes/api/employees'));
     app.use('/spaceStation', require('./routes/api/spaceStation'));
 
@@ -34,17 +28,6 @@ async function startServer() {
     app.get(/.*/, (req, res) => {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-
-    // app.all(/.*/, (req, res) => {
-    //     res.status(404);
-    //     if (req.accepts('html')) {
-    //         res.sendFile(path.join(__dirname, 'public', '404.html'));
-    //     } else if (req.accepts('json')) {
-    //         res.json({ error: "404 Not Found" });
-    //     } else {
-    //         res.type('txt').send("404 Not Found");
-    //     }
-    // }); 
 
     startSpaceFetchInterval();
     
